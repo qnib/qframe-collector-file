cb27e58864e2f47eb4d5073b1706552008932c40
Christian Kniep <christian@qnib.org>
2017-04-09 10:03:06 +0200

    add main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"C"
+	"log"
+
+	"github.com/hpcloud/tail"
+	"github.com/qnib/qframe/types"
+	"github.com/zpatrick/go-config"
+)
+
+func Run(qChan qtypes.QChan, cfg config.Config) {
+	log.Println("[II] Start file collector")
+	fPath, err := cfg.String("collector.file.path")
+	if err != nil {
+		log.Println("[EE] No file path for collector.file.path set")
+		return
+	}
+	fileReopen, err := cfg.BoolOr("collector.file.reopen", true)
+	t, err := tail.TailFile(fPath, tail.Config{Follow: true, ReOpen: fileReopen})
+	if err != nil {
+		log.Printf("[WW] File collector failed to open %s: %s", fPath, err)
+	}
+	for line := range t.Lines {
+		qm := qtypes.NewQMsg("collector", "fPath")
+		qm.Msg = line.Text
+		qChan.Data.Send(qm)
+	}
+}
